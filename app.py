5ba893a32208bd05f30307db2c1fcd52fda279c9
wilsonzhang1913 <wilsonzhangucla@gmail.com>
2025-04-27 14:34:27 -0700

    update web page background to moving stars

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/author")
+def author():
+    return render_template("author.html")
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
