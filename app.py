0ee98a94911dcb0e4096743a652038a9f66eaf24
Wilson Zhang <wzhang3@atlassian.com>
2025-05-04 18:00:22 -0700

    update

--- app.py
+++ app.py
@@ -96,5 +96,5 @@ def chat_stream():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8083))
     app.run(host="0.0.0.0", port=port)
